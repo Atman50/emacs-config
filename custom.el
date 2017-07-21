3d6b351f3a74ca38d04c84b21ba4d8de1f9678cf
Adam Taylor <adtaylor@carboniteinc.com>
2017-07-21 08:04:23 -0400

    Major facelift
    Nagging problem: org-mode hooks don't get added...

--- custom.el
+++ custom.el
@@ -1,9 +1,14 @@
-;;; custom.el --- A place for my custom settings
+;;; custom.el --- Your emacs custom file.
 
 ;;; Commentary:
-;;    My Emacs custom settings
+;;;    Where your settings are saved. This file is pointed
+;;;    to by the variable `custom-file'
 
 ;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C U S T O M I Z A T I O N  section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -11,36 +16,103 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(browse-url-browser-function (quote browse-url-chrome))
+ '(browse-url-chrome-program "chrome")
+ '(browse-url-mozilla-program "/usr/locla/bin/chrome")
  '(c-tab-always-indent nil)
+ '(case-fold-search t)
  '(column-number-mode t)
+ '(compilation-error-regexp-alist (quote (ant bash java gnu gcc-include lcc perl)))
+ '(compilation-error-regexp-systems-list (quote (gnu perl)))
  '(confirm-kill-emacs (quote yes-or-no-p))
  '(custom-safe-themes
    (quote
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(diff-switches "-bc")
  '(dired-listing-switches "-alh")
+ '(ediff-split-window-function (quote split-window-vertically))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(electric-pair-mode t)
  '(eshell-history-size 1024)
  '(fill-column 78)
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(flycheck-flake8-maximum-line-length 200)
  '(font-lock-maximum-size nil)
+ '(git-commit-style-convention-checks (quote (non-empty-second-line overlong-summary-line)))
  '(global-font-lock-mode t nil (font-lock))
+ '(history-delete-duplicates t)
+ '(history-length t)
  '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(ivy-count-format "")
+ '(ivy-height 16)
+ '(ivy-mode t)
+ '(ivy-use-virtual-buffers t)
+ '(line-move-visual nil)
+ '(line-number-display-limit-width 40000)
+ '(magit-pull-arguments nil)
+ '(magit-repository-directories (quote (("~/repos" . 1))))
+ '(make-backup-files nil)
+ '(omnisharp-auto-complete-popup-help-delay 1)
+ '(org-catch-invisible-edits (quote show))
+ '(org-clock-into-drawer "CLOCKING")
+ '(org-log-done (quote time))
+ '(org-log-into-drawer t)
+ '(org-todo-keyword-faces
+   (quote
+    (("DONE" :foreground "forest green" :weight bold)
+     ("PROG" :foreground "dark blue" :weight bold)
+     ("STBY" :foreground "DarkOrange3" :weight bold))))
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t/!)" "WAIT(w/!)" "PROG(p/!)" "STBY(s/!)" "ASSIGNED(a/!)" "|" "DONE(d/!)" "COMPLETE(c/!)"))))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://melpa.org/packages/")
      ("org" . "http://orgmode.org/elpa/"))))
+ '(powerline-display-hud nil)
+ '(projectile-completion-system (quote ivy))
+ '(projectile-mode-line (quote (:eval (format " [%s]" (projectile-project-name)))))
+ '(python-indent-trigger-commands (quote (yas-expand yas/expand)))
+ '(python-shell-completion-setup-code "from IPython.core.completerlib import module_completion")
+ '(python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))
+")
+ '(python-shell-interpreter-args "-i -q")
+ '(python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: ")
+ '(python-shell-prompt-regexp "In \\[[0-9]+\\]: ")
+ '(savehist-additional-variables (quote (kill-ring search-ring regexp-search-ring)))
+ '(savehist-file "~/.emacs.d/savehist")
+ '(savehist-mode t)
+ '(scroll-bar-mode (quote none))
+ '(sentence-end-double-space nil)
  '(show-paren-mode t nil (paren))
+ '(split-width-threshold nil)
+ '(tool-bar-mode nil)
+ '(truncate-partial-width-windows t)
  '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify))
+ '(use-dialog-box t)
  '(use-package-always-defer t)
  '(use-package-always-ensure t)
- '(which-key-mode t))
+ '(use-package-verbose t)
+ '(vc-diff-switches (quote ("--diff-cmd" "tkdiff")))
+ '(visible-bell t)
+ '(which-function-mode t)
+ '(which-key-mode t)
+ '(yas-global-mode t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:slant normal :weight normal :height 98 :width normal :foundry "outline" :family "DejaVu Sans Mono"))))
+ '(Info-quoted ((t (:foreground "red3"))))
+ '(powerline-active1 ((t (:inherit mode-line :background "dark salmon"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "dark turquoise"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "dark gray"))))
+ '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "dim gray"))))
+ '(region ((t (:background "sky blue" :distant-foreground "gtk_selection_fg_color")))))
 
-;; custom.el ends here
+;;; custom.el ends here
--- init.el
+++ init.el
@@ -4,10 +4,22 @@
 
 ;;; Commentary:
 ;;    My Emacs configuration
+;;
+;;    Most customize-able settings have been moved into the custom.el file.
+;;    The packages are configured via their customizable variables. Use the
+;;    M-x customize-group and M-x customize-variable for help. You can also
+;;    just look through the custom.el file directly.
+;;
 
 ;;; Code:
 
 (package-initialize)
+
+;; Output load time to messages
+(add-hook 'after-init-hook
+          (lambda ()
+            (message "init completed in %.2fs"
+                     (float-time (time-subtract after-init-time before-init-time)))))
 
 ;; Your customizations are stored in the custom.el file
 ;;    *DO THIS FIRST SO YOU CAN CUSTOMIZE PACKAGES BEFORE LOADING THEM*
@@ -18,6 +30,10 @@
 ;;              Setup the package stuff                   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Package paths are setup via custom.el
+(package-refresh-contents)
+
+;; Make sure use-package is installed
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
@@ -43,15 +59,19 @@
   (use-package s)
   (use-package shut-up)
   (load-library "omnisharp")
-  
+
   (use-package csharp-mode
     :config
     (progn
       (add-hook 'csharp-mode-hook 'company-mode)
       (add-hook 'csharp-mode-hook 'omnisharp-mode))))
 
-;; other packages
-(use-package terraform-mode)
+;; For powershell on dark background you might need to customize faces
+(use-package powerline
+  :demand
+  :config
+  (powerline-default-theme))
+
 (use-package bind-key)
 (use-package git-commit)
 (use-package magit)
@@ -59,38 +79,35 @@
 (use-package magit-find-file)
 (use-package git-timemachine)
 
-(use-package org
+(use-package org-plus-contrib
   :pin org
-  :config (progn
-            (add-hook 'org-mode-hook (lambda() (turn-on-auto-fill)))
-            (add-hook 'org-mode-hook 'org-bullets-mode)))
-(use-package org-plus-contrib :pin org)
+  :config
+  (progn
+    (add-hook 'org-mode-hook (lambda() (turn-on-auto-fill)))
+    (add-hook 'org-mode-hook 'org-bullets-mode)))
+
 (use-package org-autolist)
 
-(use-package magit-popup)
+(use-package terraform-mode)
 (use-package protobuf-mode)
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Python configuration
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Get the python root based on the system we are running under.
-;; This, of course, uses Python36 as the default version.
 
 ;; You'll need anaconda-mode in your python (via pip). Install ipython, anaconda-mode, pyflake8/flake8, pylint
 (use-package anaconda-mode)
 (use-package company
   :config
   (progn
-   (add-to-list 'company-backends 'company-anaconda)
-   (add-to-list 'company-backends 'company-ansible)
-   (when config/use-omnisharp
-     (add-to-list 'company-backends 'company-omnisharp))))
+    (add-to-list 'company-backends 'company-anaconda)
+    (when config/use-omnisharp
+      (add-to-list 'company-backends 'company-omnisharp))))
 (use-package company-anaconda)
 (use-package flycheck
   :config
-  (global-flycheck-mode))
+  (progn
+    (global-flycheck-mode)))
 
 (use-package flycheck-pyflakes) ;; flycheck uses flake8!
 (use-package pylint)
@@ -119,13 +136,12 @@
 
 (use-package yasnippet
   :demand
+  :diminish (yas-minor-mode . "")
   :config (progn
-            (yas-global-mode t)
             (yas-reload-all)
             ;; Setup to allow for yasnippets to use code to expand
             (require 'warnings)
             (add-to-list 'warning-suppress-types '(yasnippet backquote-change))))
-(use-package yatemplate)
 
 ;; Fix yas indent issues
 (add-hook 'python-mode-hook
@@ -138,22 +154,24 @@
 ;; Projectile
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package projectile
-  :config (progn
-            (setq projectile-completion-system 'ivy)))
+  :demand)
+;; Somehow projectile mode being saved in the custom.el saves the setting
+;; as '(projectile-mode t nil (projectile)) which causes issues when reloading
+;; the elpa packages - fails because when custom.el is loading it doesn't know
+;; about (projectile).
+(projectile-mode t)
 
 (use-package org-projectile)
 (use-package ibuffer-projectile)
-(projectile-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Some more packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package xterm-color)
-(use-package which-key)
+(use-package which-key
+  :demand
+  :diminish "")
 (use-package sh-script)
-
-;; To use this package you'll need silversearcher (ag) installed on your system.
-(use-package ag)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; DESKTOP STICKINESS
@@ -164,35 +182,29 @@
 ;; have it's own "state".
 ;; Turn this on by setting desktop-save-mode
 (use-package desktop
- :config
- (set-variable 'desktop-path (cons default-directory desktop-path)))
+  :config
+  (set-variable 'desktop-path (cons default-directory desktop-path)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; IVY CONFIG
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package ivy
-  :diminish (ivy-mode . "")
+  :diminish ""
+  :demand
   :config
-  (ivy-mode 1)
-  ;; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’.
-  (setq ivy-use-virtual-buffers t)
-  ;; number of result lines to display
-  (setq ivy-height 16)
-  ;; no counting
-  (setq ivy-count-format "")
-  ;; no regexp by default
-  (setq ivy-initial-inputs-alist nil)
-  ;; configure regexp engine.
-  (setq ivy-re-builders-alist
-        ;; allow input not in order
-        '((t   . ivy--regex-ignore-order))))
+  (progn
+    ;; no regexp by default
+    (setq ivy-initial-inputs-alist nil)
+    ;; configure regexp engine.
+    (setq ivy-re-builders-alist
+          ;; allow input not in order
+          '((t . ivy--regex-ignore-order)))))
 
 ;; We can use counsel with ivy
 (use-package counsel)
 (use-package counsel-projectile)
 (use-package swiper
- 
   :init (ivy-mode 1)
   :bind (("C-c s" . swiper)))
 
@@ -202,8 +214,7 @@
 (defun my-ansi-term (term-name cmd)
   "Create an ansi term with a name - other than *ansi-term* given TERM-NAME and CMD."
   (interactive "sName for terminal: \nsCommand to run [/bin/bash]: ")
-  (if (= 0 (length cmd)) (setq cmd "/bin/bash"))
-  (ansi-term cmd)
+  (ansi-term (if (= 0 (length cmd)) "/bin/bash" cmd))
   (rename-buffer term-name))
 
 ;; Get bash/python file definitions. This fixes the problem where
@@ -225,7 +236,7 @@
         (error nil))))
 (add-hook 'find-file-hook 'my-find-file-hook)
 
-;; Run elisp with eldoc-mode and paredit-mode (not sure about paredit these days)
+;; Run elisp with eldoc-mode
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
 ;; prefer ibuffer over list-buffers
@@ -233,6 +244,12 @@
 
 ;; for lazy people use y/n instead of yes/no
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;; Diminish some of the modes
+(diminish 'eldoc-mode "Doc")
+
+;; Use hippie completion
+(bind-key "M-/" 'hippie-expand)
 
 ;; handy way of getting back to previous places
 (bind-key "C-x p" 'pop-to-mark-command)
@@ -264,9 +281,6 @@
 (bind-key "<up>" 'enlarge-window ctl-x-map)
 (bind-key "<down>" 'shrink-window ctl-x-map)
 
-;; Turn on nice mode line which-function-mode
-(which-function-mode t)
-
 ;; For ediff mode
 (setq-default ediff-ignore-similar-regions t)
 
@@ -279,18 +293,18 @@
 ;; Stolen from Sacha Chua's emacs setup
 ;;    http://pages.sachachua.com/.emacs.d/Sacha.html
 ;; Nice history
+;;  history settings are in history and history/savehist customized settings
 (require 'savehist)
-(setq savehist-file (expand-file-name "savehist" user-emacs-directory))
-(savehist-mode 1)
-(setq history-length t)
-(setq history-delete-duplicates t)
-(setq savehist-additional-variables
-      '(kill-ring
-        search-ring
-	regexp-search-ring))
 
-;; Stuff to do after custom macros
-(use-package smart-mode-line)
-(smart-mode-line-enable)
+;; This I picked up from searching web because of emacs.exe.stackdump files
+;; repeatedly getting created in the directories I was working in. Seems the
+;; console somehow intereacts poorly with windows unless these changes are made
+(if (eq window-system 'w32)
+    (progn
+      (add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
+      (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt nil t)
+      (setq explicit-shell-file-name (if (eq system-type 'windows-nt)  "C:\\cygwin64\\bin\\bash.exe" "bash.exe"))
+      (setq shell-file-name explicit-shell-file-name)))
+
 
 ;;; init.el ends here
