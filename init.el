eae9e2626ce0d3ccd0fa4b315a442cb857fedf1a
Adam Taylor <adtaylor@carboniteinc.com>
2018-01-23 14:28:12 -0500

    First stab at a literate configuration

--- init.el
+++ init.el
@@ -4,19 +4,16 @@
 ;; Copyright (C) 2017-2018 Adam Taylor
 
 ;;; Commentary:
-;;    My Emacs configuration. Not trying to make everyone else like me but
-;;    this might be a handy starting point for others.
+;;      My emacs configuration. Here there is mostly glue code for literate initialization
+;;          1. Initialize the package system
+;;          2. Load a separate custom.el file (before any packages are actually loaded).
+;;             This allows for variables to be overriden via custom-set-variables.
+;;          3. Setup some optimizations for heavy package loading
+;;          4. Fix up some broken windows package encoding issues (melpa)
+;;          5. Load up use-package
+;;          6. Load up org mode
+;;          7. org-babel-load-file README.org
 ;;
-;;    Some benefits of this configuration:
-;;       - Most settings are in the custom.el file so it's easy to change the
-;;         look and feel without having to edit this file. Just customize group
-;;         and face
-;;       - Extensive use of use-package allows this init file to be portable: if
-;;         you don't have the packages installed on the target system, they will
-;;         automatically install
-;;       - This makes it easier to keep init.el cleaner
-;;
-;;    Feel free to use all or part of this configuration
 
 ;;; Code:
 
@@ -30,13 +27,11 @@
 ;; Turn down the garbage collector during the loading of this file in case use-package
 ;; has do some compiling. Set it back when done with init.
 (setq gc-cons-threshold 64000000)
-(add-hook 'after-init-hook #'(lambda ()
-                               ;; restore after startup
-                               (setq gc-cons-threshold 800000)))
+(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;              Setup packages                  ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       Setup packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Fixes melpa package load issues with some unicode characters it doesn't know how to
 ;; deal with (incorrect or missing https://en.wikipedia.org/wiki/Byte_order_mark)
@@ -49,249 +44,10 @@
   (package-install 'use-package))
 (require 'use-package)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                     PACKAGES                           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package auto-compile
-  :config (auto-compile-on-load-mode))
-(use-package diminish)
-(use-package bind-key)
-(use-package savehist :demand t)                ;; Nice history in ~/.emacs.d/savehist
-(use-package ag)                                ;; silversearcher
-(use-package powershell
-  :if mswindows-p)
-
-;; To use omnisharp follow directions below
-;; Load up local omnisharp (roslyn flavor) - set the load-path to where you've put
-;; the omnisharp-emacs repo: https://github.com/OmniSharp/omnisharp-emacs.git
-;; this site also contains all of the directions for getting omnisharp running.
-(defvar config/use-omnisharp nil)
-(let ((omnisharp (car (get 'omnisharp-server-executable-path 'saved-value))))
-  (unless (null omnisharp)
-    (setq config/use-omnisharp (file-exists-p omnisharp))))
-(use-package omnisharp
-  :diminish "\u221e"                            ;; infinity symbol
-  :if config/use-omnisharp
-  :bind (:map omnisharp-mode-map
-              ("C-c o" . omnisharp-start-omnisharp-server)
-              ("C-c d" . omnisharp-go-to-definition-other-window)
-              ("C-c C-j" . counsel-imenu))
-  :config
-  (setq omnisharp-debug t))
-(use-package csharp-mode
-  :config
-  (when config/use-omnisharp
-    (add-hook 'csharp-mode-hook 'company-mode)
-    (add-hook 'csharp-mode-hook 'omnisharp-mode)))
-
-(load-theme 'leuven t)                          ;; Theme: works better before powerline
-(use-package powerline
-  :demand t
-  :config
-  (powerline-default-theme))
-
-(use-package git-commit)
-(use-package magit
-  :demand t
-  :bind (("C-c f" . magit-find-file-other-window)
-         ("C-c g" . magit-status)
-         ("C-c l" . magit-log-buffer-file))
-  ;; Make the default action a branch checkout, not a branch visit when in branch mode
-  :bind (:map magit-branch-section-map
-              ([remap magit-visit-thing] . magit-branch-checkout)))
-(use-package magit-filenotify)
-(use-package magit-find-file)
-(use-package git-timemachine)
-
+;; Literate emacs configuration requires org-babel-load-file
 (use-package org
   :demand t
   :pin "org")
-(use-package org-bullets
-  :demand t
-  :config (add-hook 'org-mode-hook 'org-bullets-mode))
-(use-package org-autolist)
-
-(add-to-list 'auto-mode-alist
-             '("\\.aspx\\'" . html-mode)
-             '("\\.aspcx\\'" . html-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Python configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package company
-  :diminish "Co"
-  :config
-  (when config/use-omnisharp
-    (add-to-list 'company-backends 'company-omnisharp)))
-(use-package company-jedi)
-(use-package elpy
-  :demand t
-  :config
-  (progn
-    (elpy-enable)
-    (add-hook 'elpy-mode-hook
-              '(lambda ()
-                 (progn
-                   (setq-local flymake-start-syntax-check-on-newline t)
-                   (setq-local flymake-no-changes-timeout 0.5))))))
-(use-package flycheck
-  :diminish  "\u2714"           ;; heavy checkmark
-  :config
-  (global-flycheck-mode))
-(use-package flycheck-pyflakes) ;; flycheck uses flake8!
-(use-package pylint)
-(use-package python-docstring
-  :config
-  (python-docstring-install))
-(use-package python
-  :config
-  (progn
-    (add-hook 'python-mode-hook '(lambda () (add-to-list 'company-backends 'company-jedi)))
-    (add-hook 'python-mode-hook 'flycheck-mode)
-    (add-hook 'python-mode-hook 'company-mode)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; End of python configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package realgud :demand t)                 ;; Better than gud?
-(use-package projectile :demand t :config (projectile-mode t))
-(use-package org-projectile)
-(use-package ibuffer-projectile)
-(use-package xterm-color)
-(use-package which-key :demand t :diminish "")
-(use-package sh-script)
-(use-package desktop
-  :config
-  ;; put desktop in start directory
-  (set-variable 'desktop-path (cons default-directory desktop-path)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Ivy configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package ivy
-  :demand t
-  :diminish ""
-  :bind (:map ivy-minibuffer-map
-              ("C-w" . ivy-yank-word)           ;; make work like isearch
-              ("C-r" . ivy-previous-line))
-  :config
-  (progn
-    (setq ivy-initial-inputs-alist nil)         ;; no regexp by default
-    (setq ivy-re-builders-alist                 ;; allow input not in order
-          '((t . ivy--regex-ignore-order)))))
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x g" . counsel-git)
-         ("C-x C-f" . counsel-find-file)
-         ("C-c C-j" . counsel-imenu))
-  :bind (:map help-map
-              ("f" . counsel-describe-function)
-              ("v" . counsel-describe-variable)
-              ("b" . counsel-descbinds)))
-(use-package counsel-projectile)
-(use-package counsel-etags)
-(use-package ivy-hydra)
-(use-package swiper
-  :bind (("C-S-s" . isearch-forward)
-         ("C-s" . swiper)
-         ("C-S-r" . isearch-backward)
-         ("C-r" . swiper)))
-(use-package avy)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; End of Ivy configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Fix yasnippets with other company backends:
-;;  https://emacs.stackexchange.com/questions/10431/get-company-to-show-suggestions-for-yasnippet-names
-;; Add yasnippet support for all company backends: https://github.com/syl20bnr/spacemacs/pull/179
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package yasnippet
-  :diminish (yas-minor-mode . "")
-  :config (progn
-            (yas-reload-all)
-            ;; fix tab in term-mode
-            (add-hook 'term-mode-hook (lambda() (yas-minor-mode -1)))
-            ;; Fix yas indent issues
-            (add-hook 'python-mode-hook '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
-            ;; Setup to allow for yasnippets to use code to expand
-            (require 'warnings)
-            (add-to-list 'warning-suppress-types '(yasnippet backquote-change))))
-
-(defvar company-mode/enable-yas t
-  "Enable yasnippet for all backends.")
-(defun company-mode/backend-with-yas (backend)
-  "Add in the company-yasnippet BACKEND."
-  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-      backend
-    (append (if (consp backend) backend (list backend))
-            '(:with company-yasnippet))))
-
-(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; bits-o-configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Limit the length of the current function via `which-function'
-(defvar  which-function-max-width 64 "The maximum width of the which-function string.")
-(advice-add 'which-function :filter-return
-            (lambda (s) (if (< (string-width s) which-function-max-width) s
-                          (concat (truncate-string-to-width s (- which-function-max-width 3)) "..."))))
-
-(defun my-ansi-term (term-name cmd)
-  "Create an ansi term with a name - other than *ansi-term* given TERM-NAME and CMD."
-  (interactive "sName for terminal: \nsCommand to run [/bin/bash]: ")
-  (ansi-term (if (= 0 (length cmd)) "/bin/bash" cmd))
-  (rename-buffer term-name))
-
-(defun my-find-file-hook ()
-  "If `fundamental-mode', look for script type so the mode gets properly set.
-Script-type is read from #!/... at top of file."
-  (if (eq major-mode 'fundamental-mode)
-      (condition-case nil
-          (save-excursion
-            (goto-char (point-min))
-            (re-search-forward "^#!\s*/.*/\\(python\\|bash\\).*$")
-            (if (string= (match-string 1) "python")
-                (python-mode)
-              (sh-mode)))
-        (error nil))))
-(add-hook 'find-file-hook 'my-find-file-hook)
-
-
-(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)    ;; Run elisp with eldoc-mode
-(fset 'list-buffers 'ibuffer)                   ;; prefer ibuffer over list-buffers
-(fset 'yes-or-no-p 'y-or-n-p)                   ;; for lazy people use y/n instead of yes/no
-(diminish 'eldoc-mode "Doc")                    ;; Diminish eldoc-mode
-
-;; Some key bindings
-(bind-key "C-x p" 'pop-to-mark-command)
-(bind-key "C-h c" 'customize-group)
-(bind-key "C-+" 'text-scale-increase)
-(bind-key "C--" 'text-scale-decrease)
-(bind-key "C-z" 'nil)                           ;; get rid of pesky "\C-z"
-(bind-key "C-z" 'nil ctl-x-map)                 ;;    and "\C-x\C-z" annoying minimize
-(bind-key "C-c C-d" 'dired-jump)
-(bind-key "C-c r" 'revert-buffer)
-(bind-key "C-c t" 'toggle-truncate-lines)
-(bind-key "C-c c" 'comment-region)
-(bind-key "C-c u" 'uncomment-region)
-(bind-key "<up>" 'enlarge-window ctl-x-map)     ;; note: C-x
-(bind-key "<down>" 'shrink-window ctl-x-map)    ;; note: C-x
-
-(setq-default ediff-ignore-similar-regions t)   ;; Not a variable but controls ediff
-
-;; Turn on some stuff that's normally set off
-(put 'narrow-to-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'scroll-left 'disabled nil)
+(org-babel-load-file "~/.emacs.d/README.org")
 
 ;;; init.el ends here
